d6df15b64b2be24512916aa1961e1f185052174c
ardiansetya <ardiansetya129@gmail.com>
2025-01-25 23:29:08 +0700

    create dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   return (
     <div className="container mx-auto text-white">
       <nav className=" py-10">
@@ -12,26 +14,57 @@ const Navbar = () => {
             </div>
             Epictetus
           </div>
-          <div className="w-8/12 flex gap-14">
-            <Link className="hover:underline" href={"/"}>
-              UI Design
-            </Link>
-            <Link className="hover:underline" href={"/"}>
-              Frontend
-            </Link>
-            <Link className="hover:underline" href={"/"}>
-              Backend
-            </Link>
-            <Link className="hover:underline" href={"/"}>
-              lainya{" "}
-            </Link>
+          <div className="w-8/12">
+            <ul className=" flex gap-14">
+              <li>
+                <Link className="hover:underline" href={"/"}>
+                  UI Design
+                </Link>
+              </li>
+              <li>
+                <Link className="hover:underline" href={"/"}>
+                  Frontend
+                </Link>
+              </li>
+              <li>
+                <Link className="hover:underline" href={"/"}>
+                  Backend
+                </Link>
+              </li>
+              <li>
+                <Link onClick={() => setDropdownOpen(!dropdownOpen)} className="hover:underline " href={"/"}>
+                  lainya
+                </Link>
+                <ul className="absolute mt-4 shadow-md w-[200px] bg-gray-800 rounded-md">
+                  {dropdownOpen && (
+                    <li>
+                      <Link
+                        className="px-4 py-3 flex border-b border-white/5 hover:underline hover:text-gray-400"
+                        href={"/"}>
+                        Internet
+                      </Link>
+                      <Link
+                        className="px-4 py-3 flex border-b border-white/5 hover:underline hover:text-gray-400"
+                        href={"/"}>
+                        Books
+                      </Link>
+                      <Link
+                        className="px-4 py-3 flex border-b border-white/5 hover:underline hover:text-gray-400"
+                        href={"/"}>
+                        Open Source
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </li>
+            </ul>
           </div>
           <div className="w-2/12 relative">
             <FaSearch className="absolute top-4 left-5 text-gray-400/20" />
             <input
               type="text"
               placeholder="Search"
-              className=" py-3 px-14 rounded-full bg-gray-700 w-full"
+              className=" py-3 px-14 rounded-full bg-gray-700 w-full focus:outline-none focus:ring-2 focus:ring-gray-400/20"
             />
           </div>
         </div>
