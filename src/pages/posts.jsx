a5a4574806d1f936932c06e08e9f9a733045d519
ardiansetya <ardiansetya129@gmail.com>
2025-01-29 19:37:44 +0700

    fix responsive post page

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -9,13 +9,26 @@ const posts = () => {
   return (
     <Layout>
       <SectionHeader>UI Design</SectionHeader>
-      <div className="flex flex-wrap items-center -mx-4 mt-8">
-        {posts.map((post) => (
-          <div key={post.id} className="md:w-4/12 w-full px-4 py-6">
-            <CardPost {...post} />
+
+      {!posts.lenght? (
+        
+          <div className="flex flex-wrap items-center -mx-4 mt-8">
+            {posts.map((post) => (
+              <div key={post.id} className="md:w-4/12 w-full px-4 py-6">
+                <CardPost {...post} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        
+      ) : (
+        <div className="text-center py-20">
+          <h1 className="sm:text-6xl text-4xl">No Result 😢</h1>
+          <p className="sm:text-xl text-lg text-white/60 mt-4 w-full md:w-6/12 mx-auto">
+            We couldn't find any post with the keyword 'asdasdasd'. Please try
+            another keyword
+          </p>
+        </div>
+      )}
     </Layout>
   );
 };
